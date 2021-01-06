59b61b3fcf24a5dfbc8aebf6bee7403121235580
Mohamed Samir <robben.97@hotmail.com>
2021-01-06 18:24:43 +0200

    Express router

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express =require("express");
 const http =require("http");
 const morgan= require("morgan");
 const bodyParser = require('body-parser');
-
+const dishRouter = require('./routes/dishRouter');
 
 
 const hostname ='localhost';
@@ -12,56 +12,9 @@ const app =express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
-app.all('/dishes', (req,res,next) =>{
-    res.statusCode =200;
-    res.setHeader('Content-Type','text/plain');
-    next();
-});
-app.get('/dishes',(req,res,next) =>{
-    res.end('Will send all the dishes to you !');  
 
-});
 
-app.post('/dishes',(req,res,next) =>{
-    res.end('will add the dish: '+ req.body.name+ 
-    'with details' +req.body.description);
-
-});
-
-app.put('/dishes',(req,res,next) =>{
-    res.statusCode =403;
-    res.end('put operation not supported on /dishes');
-
-});
-
-app.delete('/dishes',(req,res,next) =>{
-    res.end('deleting all the dishes!');  
-
-});
-
-app.get('/dishes/:dishId',(req,res,next) =>{
-    res.end('Will send details of the dish :'+req.params.dishId + ' to you');  
-
-});
-
-app.post('/dishes/:dishId',(req,res,next) =>{
-    res.statusCode =403;
-    res.end('post operation not supported on /dishes/'+ req.params.dishId);
-
-});
-
-app.put('/dishes/:dishId',(req,res,next) =>{
-    res.write(' Updating the dish:'+req.params.dishId+'\n');
-    res.end('will update the dish:' +req.body.name+
-    'with details: '+req.body.description);
-
-});
-
-app.delete('/dishes/:dishId',(req,res,next) =>{
-    res.end('deleting dish: '+req.params.dishId);  
-
-});
-
+app.use('/dishes', dishRouter);
 app.use(express.static(__dirname+'/public'))
 
 app.use((req, res, next)=> {
